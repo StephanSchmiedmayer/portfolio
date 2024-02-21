437a3a0c63ac1ac93f4f7882e0ee34272c46dcac
Stephan Schmiedmayer <56692976+StephanSchmiedmayer@users.noreply.github.com>
2024-02-21 20:58:19 +0100

    Fix navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './portfolio/templates/*.html'
+    './portfolio/templates/*.html',
+    './portfolio/templates/**/*.html',
+    './portfolio/templates/*.css',
   ],
   theme: {
     extend: {},
